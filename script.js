0a7dce67597c407c697bfca6c3df409bc84878bd
dhruvilad1001 <dhruvilad1001@gmail.com>
2025-07-29 12:19:33 +0530

    Updated homepage layout and styles

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let a = new Date
-console.log(a.getHours())
-console.log(a.getMinutes())
+let months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let hours=document.getElementById("hrs")
+let minuets=document.getElementById("min")
+let seconds=document.getElementById("sec")
+let date=document.getElementById("date")
+let month=document.getElementById("mon")
+let day=document.getElementById("day")
+let year=document.getElementById("year")
+let ampm=document.getElementById("am-pm")
+
+setInterval(() => {
+let time=new Date
+hours.innerHTML=(time.getHours()<10?"0":"") + time.getHours()
+minuets.innerHTML=(time.getMinutes()<10?"0":"") + time.getMinutes()
+seconds.innerHTML=(time.getSeconds()<10?"0":"") + time.getSeconds()
+date.innerHTML=time.getDate()
+month.innerHTML=months[time.getMonth()]
+day.innerHTML=days[time.getDay()]
+year.innerHTML=time.getFullYear()
+ampm.innerHTML=time.getHours()>=12?"PM":"AM"
+},1000);
