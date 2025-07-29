0cbffdc364890bead5689ad21865dbd5671b688d
dhruvilad1001 <dhruvilad1001@gmail.com>
2025-07-29 14:12:32 +0530

    Updated homepage layout and styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let months=["January","February","March","April","May","June","July","August","September","October","November","December"]
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 let hours=document.getElementById("hrs")
 let minuets=document.getElementById("min")
